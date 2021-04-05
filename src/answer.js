f26c0427b1e3e3ebedbea37bdcb64158130c9f21
dvrandecic <dvrandecic@wikimedia.org>
2021-04-04 17:44:01 -0700

    made a bit more robust on REPL too

--- src/answer.js
+++ src/answer.js
@@ -4,14 +4,18 @@ const https = require('https');
 
 const labelize = require('./labelize.js');
 const load = require('./load.js');
+const utils = require('./utils.js');
 
 const answer = (command, callback) => {
   const data = command.trim();
   const first = data[0];
   if (first === '[' || first === '{' || first === '"') {
     labelize.labelize(JSON.parse(data)).then(callback);
-  } else {
+  } else if (utils.is_zid(data)) {
     load.load(data).then(labelize.labelize).then(callback);
+  } else {
+    console.log('Did not understand input.');
+    callback(data);
   }
 }
 
