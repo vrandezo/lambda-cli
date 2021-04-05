9ce073fee591ce31ce6ec4a1a8f1f0a55d1e83fa
dvrandecic <dvrandecic@wikimedia.org>
2021-04-05 14:37:04 -0700

    just use the internal

--- src/prettyprint.js
+++ src/prettyprint.js
@@ -3,77 +3,10 @@
 const canonicalize = require('./canonicalize.js');
 const utils = require('./utils.js');
 
-const out = (s) => {
-  process.stdout.write(s);
-}
-
-const nl = () => {
-  console.log('');
-}
-
-const spaces = (x) => {
-  out('\t'.repeat(x));
-}
-
-const prettyprint_object = (o, indent) => {
-  console.log('{');
-  let first = true;
-  for (let key of Object.keys(o)) {
-    if (!first) {
-      out(',');
-      nl();
-    }
-    first = false;
-    spaces(indent+1);
-    out('"');
-    out(key);
-    out('": ');
-    prettyprint_internal(o[key], indent+1);
-  }
-  nl();
-  spaces(indent);
-  out('}');
-}
-
-const prettyprint_string = (s, indent) => {
-  out('"' + s + '"');
-}
-
-const prettyprint_array = (a, indent) => {
-  if (a.length === 0) {
-    out('[]');
-    return;
-  }
-  out('[');
-  nl();
-  let first = true;
-  for (let item of a) {
-    if (!first) {
-      out(',');
-      nl();
-    }
-    first = false;
-    spaces(indent+1);
-    prettyprint_internal(item, indent+1);
-  }
-  nl();
-  spaces(indent);
-  out(']');
-}
-
-const prettyprint_internal = (zobject, indent) => {
-  if (utils.is_object(zobject)) {
-    prettyprint_object(zobject, indent);
-  } else if (utils.is_string(zobject)) {
-    prettyprint_string(zobject, indent);
-  } else if (utils.is_array(zobject)) {
-    prettyprint_array(zobject, indent);
-  }
-}
-
 const prettyprint = (zobject) => {
-  const result = canonicalize.canonicalize(zobject);
-  prettyprint_internal(result, 0);
+  process.stdout.write(
+    JSON.stringify(canonicalize.canonicalize(zobject), null, '\t')
+  );
   return null;
 }
 
